85bff36ed7554f33cbcb1872dcf3a67230756724
Venky-2107 <chvenkatpatiraju@gmail.com>
2023-09-06 21:08:45 +0530

    added get method to get all users

--- backend/src/controllers/adminLoginController.js
+++ backend/src/controllers/adminLoginController.js
@@ -1,18 +1,25 @@
-const Login = require('../models/adminRegisterModel');
+const admin_login = require("../models/adminRegisterModel");
+const uuid = require("uuid");
 
-const loginController = {}
+const loginController = {};
 
-loginController.registerUser = async function(req,res){
-    try{
-        let newUser = new Login({
-            ...req.body
-        })
-        newUser.save();
-        res.status(200).send({status : "Successfully uploaded", data : newUser});
-    }
-    catch(err){
-        res.status(400).send({status : "Failed"});
-    }
-}
+loginController.registerUser = async function (req, res) {
+  try {
+    let newUser = new admin_login({ _id: uuid.v4(), ...req.body });
+    newUser.save();
+    res.status(200).send({ status: "Successfully uploaded", data: newUser });
+  } catch (err) {
+    res.status(400).send({ status: "Failed" });
+  }
+};
+
+loginController.getAdminUser = async (req, res) => {
+  try {
+    let users = await admin_login.find({});
+    res.status(200).send({ status: "Successfully Fetched", data: users });
+  } catch (err) {
+    res.status(400).send({ status: "failed" });
+  }
+};
 
 module.exports = loginController;
