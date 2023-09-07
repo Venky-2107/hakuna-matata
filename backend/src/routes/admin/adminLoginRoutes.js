96f254286682a275fd6a2b3763b684ddddd374bf
Venky-2107 <chvenkatpatiraju@gmail.com>
2023-09-07 19:25:00 +0530

    crud operations

--- backend/src/routes/admin/adminLoginRoutes.js
+++ backend/src/routes/admin/adminLoginRoutes.js
@@ -4,5 +4,9 @@ const loginController = require("../../controllers/adminLoginController");
 
 router.post("/login", loginController.registerUser);
 router.get("/users", loginController.getAdminUser);
+router.delete("/users", loginController.deleteUsers);
+router.get("/users/:id", loginController.getUserById);
+router.put("/users/:id", loginController.updateUserById);
+router.delete("/users/:id", loginController.deleteUserById);
 
 module.exports = router;
